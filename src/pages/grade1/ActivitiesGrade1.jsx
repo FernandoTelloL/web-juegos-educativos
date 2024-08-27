4772a6dc5a9cefc979bbea040ae0602f0f459bd6
Fernando Tello Leon <fernando_tellol@hotmail.com>
2024-08-27 13:22:10 -0500

    refactorizado cambios en el color de botones de grade1

--- src/pages/grade1/ActivitiesGrade1.jsx
+++ src/pages/grade1/ActivitiesGrade1.jsx
@@ -1,15 +1,34 @@
-import { Outlet } from "react-router-dom"
-import { Link } from "react-router-dom"
-
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const ActivitiesGrade1 = () => {
   return (
     <section className="my-32 container mx-auto px-4 md:w-[80%]">
       <div className="flex flex-wrap justify-center gap-4 mb-20">
-        <Link to="matematicas" className="menu-button bg-blue-500 hover:bg-blue-400 rounded-lg text-white py-3 px-6 text-lg font-semibold transition-colors duration-300">Matemáticas</Link>
-        <Link to="comunicacion" className="menu-button bg-green-500 hover:bg-green-400 rounded-lg text-white py-3 px-6 text-lg font-semibold transition-colors duration-300">Comunicación</Link>
-        <Link to="memoria" className="menu-button bg-yellow-500 hover:bg-yellow-400 rounded-lg text-white py-3 px-6 text-lg font-semibold transition-colors duration-300">Memoria</Link>
-        <Link to="concentracion" className="menu-button bg-red-500 hover:bg-red-400 rounded-lg text-white py-3 px-6 text-lg font-semibold transition-colors duration-300">Concentración</Link>
+        <Link 
+          to="matematicas" 
+          className="menu-button bg-blue-500 hover:bg-blue-600 rounded-lg text-white py-3 px-6 text-lg font-semibold transition-all duration-300 transform hover:scale-105"
+        >
+          Matemáticas
+        </Link>
+        <Link 
+          to="comunicacion" 
+          className="menu-button bg-green-500 hover:bg-green-600 rounded-lg text-white py-3 px-6 text-lg font-semibold transition-all duration-300 transform hover:scale-105"
+        >
+          Comunicación
+        </Link>
+        <Link 
+          to="memoria" 
+          className="menu-button bg-yellow-500 hover:bg-yellow-600 rounded-lg text-white py-3 px-6 text-lg font-semibold transition-all duration-300 transform hover:scale-105"
+        >
+          Memoria
+        </Link>
+        <Link 
+          to="concentracion" 
+          className="menu-button bg-red-500 hover:bg-red-600 rounded-lg text-white py-3 px-6 text-lg font-semibold transition-all duration-300 transform hover:scale-105"
+        >
+          Concentración
+        </Link>
       </div>
 
       {/* Aquí se mostrará el componente correspondiente al juego seleccionado */}
@@ -17,5 +36,5 @@ export const ActivitiesGrade1 = () => {
         <Outlet />
       </div>
     </section>
-  )
-}
+  );
+};
